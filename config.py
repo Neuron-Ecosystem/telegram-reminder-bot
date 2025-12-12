91cd095b034c59962fc89e54752dec713da1d2d5
Neuron <wertq6306@gmail.com>
2025-12-12 21:04:35 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,4 +6,6 @@ TELEGRAM_TOKEN = "8547781802:AAEZyuh1JlIAfUOCFwMNINU_cGBPXoJkjZ4"
 # URL для PostgreSQL, которую предоставит Render (переменная окружения)
 DB_URL = "ПУСТАЯ_ЗАГЛУШКА_POSTGRES_URL"
 
+DISPATCH_INTERVAL = 10
 # УДАЛЕНО: VK_TOKEN и VK_GROUP_ID
+
