ae915205e8cc3f21c0158a1a333ee3b632028c80
Neuron <wertq6306@gmail.com>
2025-12-12 21:13:15 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,9 @@
 TELEGRAM_TOKEN = "8547781802:AAEZyuh1JlIAfUOCFwMNINU_cGBPXoJkjZ4"
 
 # URL для PostgreSQL, которую предоставит Render (переменная окружения)
-DB_URL = "ПУСТАЯ_ЗАГЛУШКА_POSTGRES_URL"
+DB_URL = "postgresql://reminders_bexu_user:fvkdMpg2PdQwQ2pAZraFJJlnigFNkra3@dpg-d4u53k7gi27c73fdgm8g-a.frankfurt-postgres.render.com/reminders_bexu"
 
 DISPATCH_INTERVAL = 10
 # УДАЛЕНО: VK_TOKEN и VK_GROUP_ID
+
 
